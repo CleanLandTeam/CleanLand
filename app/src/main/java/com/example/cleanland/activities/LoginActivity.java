b4800ea79205af155874b68470a70fbfc286808b
sabbagh99 <a.sameer1999@gmail.com>
2021-06-10 20:21:18 +0300

    create new branch

---
+++ app/src/main/java/com/example/cleanland/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.cleanland.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cleanland.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
